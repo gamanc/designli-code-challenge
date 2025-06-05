06ae9d6942509e4475d475e2612ebfcd3081170f
Emerson N <gammanolasco7@gmail.com>
2025-06-05 00:51:41 -0600

    init: first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import StockDashboard from "./components/StockDashboard";
+
+export default function App() {
+  return (
+    <Box>
+      <StockDashboard />
+    </Box>
+  );
+}
