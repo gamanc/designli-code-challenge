caadd2ba0d5947478fda7fde9c00dca4be2d01ea
Emerson N <gammanolasco7@gmail.com>
2025-06-06 11:12:27 -0600

    feat: force dark mode on chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "./components/ui/provider";
 import App from "./App.tsx";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ColorModeProvider } from "./components/ui/color-mode.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider forcedTheme="dark">
+        <App />
+      </ColorModeProvider>
+    </ChakraProvider>
   </StrictMode>
 );
