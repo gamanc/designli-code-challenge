21e560ba610af5ff3056bf309679987f9ac08bf4
Emerson N <gammanolasco7@gmail.com>
2025-06-06 04:18:07 -0600

    chore: update symbols and their colors

--- src/constants/stockSymbols.ts
+++ src/constants/stockSymbols.ts
@@ -1,9 +1,17 @@
 export const SYMBOLS = [
-  "AAPL",
-  "BINANCE:BTCUSDT",
-  "COINBASE:BTC-USD",
-  "GOOGL",
-  "MSFT",
-  "AMZN",
-  "IC MARKETS:1",
+  "BINANCE:BTCUSDT", // Bitcoin (BTC) / US Dollar (USDT)
+  "BINANCE:ETHUSDT", // Ethereum (ETH) / US Dollar (USDT)
+  "BINANCE:XRPUSDT", // XRP / US Dollar (USDT)
+  "BINANCE:LTCUSDT", // Litecoin (LTC) / US Dollar (USDT)
+  "BINANCE:BCHUSDT", // Bitcoin Cash (BCH) / US Dollar (USDT)
+  "BINANCE:ADAUSDT", // Cardano (ADA) / US Dollar (USDT)
 ];
+
+export const SYMBOL_COLORS: Record<string, string> = {
+  "BINANCE:BTCUSDT": "hsl(180, 70%, 50%)",
+  "BINANCE:ETHUSDT": "hsl(45, 70%, 50%)",
+  "BINANCE:XRPUSDT": "hsl(90, 70%, 50%)",
+  "BINANCE:LTCUSDT": "hsl(135, 70%, 50%)",
+  "BINANCE:BCHUSDT": "hsl(315, 70%, 50%)",
+  "BINANCE:ADAUSDT": "hsl(225, 70%, 50%)",
+};
