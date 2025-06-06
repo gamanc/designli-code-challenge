ba4402cb6c6d56681e38d75a58c83408836257c5
Emerson N <gammanolasco7@gmail.com>
2025-06-06 04:21:18 -0600

    feat: update main page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import StockDashboard from "./components/StockDashboard";
 import { StockForm } from "./components/StockForm";
 import { Toaster } from "./components/ui/toaster";
+import { StockChart } from "./components/StockChart";
 
 export default function App() {
   return (
-    <Box>
-      <StockDashboard />
-      <StockForm />
+    <Box p={4}>
+      <Box mb={4}>
+        <StockDashboard />
+      </Box>
+
+      <Flex direction={{ base: "column", md: "row" }} gap={4}>
+        <Box minW={{ md: "300px" }} flexShrink={0}>
+          <StockForm />
+        </Box>
+
+        <Box flex="1">
+          <StockChart />
+        </Box>
+      </Flex>
+
       <Toaster />
     </Box>
   );
